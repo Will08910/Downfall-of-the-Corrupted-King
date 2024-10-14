cdfca530687a9536091f4b62127a69b5da805cee
Will08910 <550564@escg.ac.uk>
2024-10-14 16:00:47 +0100

    Upd1

---
+++ Assets/JumpScript.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class JumpScript : MonoBehaviour
+{
+    LayerMask groundLayerMask;
+
+    public GameObject player;
+
+    public bool flip;
+
+    Rigidbody2D rb;
+
+    public float speed;
+
+    private float Move;
+
+    private bool isGrounded;
+
+    public float maxSpeed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundLayerMask = LayerMask.GetMask("Ground");
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player != null)
+            {
+            DoRayCollisionCheck();
+
+            Vector2 scale = transform.localScale;
+
+            if (player.transform.position.x > transform.position.x)
+            {
+                scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+
+            else
+            {
+                scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+                transform.Translate(speed * Time.deltaTime * -1, 0, 0);
+            }
+
+            transform.localScale = scale;
+
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
+    }
+
+
+    void DoRayCollisionCheck()
+    {
+        float rayLength = 0.1f;
+
+        RaycastHit2D hit;
+
+        hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength, groundLayerMask);
+
+        Color hitColor = Color.white;
+        isGrounded = false;
+
+        if (hit.collider != null)
+        {
+            hitColor = Color.green;
+            rb.AddForce(new Vector3(0, 30, 0), ForceMode2D.Impulse);
+        }
+        Debug.DrawRay(transform.position, Vector2.down * rayLength, hitColor);
+    }
+
+    IEnumerator JumpDelay()
+    {
+        yield return new WaitForSeconds(2);
+        isGrounded = true;
+        print("num");
+    }
+}
---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int health;
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
